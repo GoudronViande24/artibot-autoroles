380226a9505b679f1e105983e2ad6991cc79cd19
Thomas Fournier <thomas@artivain.com>
2022-05-13 17:18:29 -0400

    Adapt to artibot v3

--- createrolepicker.js
+++ createrolepicker.js
@@ -1,41 +1,9 @@
-/**
- * Command to create a role picker
- * @author GoudronViande24
- * @since 2.0.0
- */
-
-const { MessageActionRow, MessageButton, TextChannel, MessageEmbed, Permissions } = require("discord.js");
-const Localizer = require("artibot-localizer");
-const path = require("path");
-const { locale } = require("../../config.json");
+import Artibot from "artibot";
+import { MessageActionRow, MessageButton, TextChannel, Permissions, Message } from "discord.js";
+import { localizer } from "./index.js";
 
 /** @type {Boolean} */
 let enabled;
-
-const localizer = new Localizer({
-	lang: locale,
-	filePath: path.resolve(__dirname, "locales.json")
-});
-
-/**
- * Send an error message
- * @param {TextChannel} channel - The channel to send the error to
- * @param {Object} config - Artibot's config
- * @param {string} reason - The error description
- * @returns {TextChannel} The error message
- */
-async function sendErrorMessage(channel, config, reason) {
-	return await channel.send({
-		embeds: [
-			new MessageEmbed()
-				.setColor("RED")
-				.setTitle("Autorole")
-				.setFooter({ text: config.botName, iconURL: config.botIcon })
-				.setTimestamp()
-				.setDescription(reason)
-		]
-	});
-};
 
 const allowedModes = [
 	"toggle",
@@ -43,58 +11,92 @@ const allowedModes = [
 	"removeonly"
 ];
 
-module.exports = {
-	name: "createrolepicker",
-	description: localizer._("Create a role picker."),
-	usage: localizer._("<list of buttons to create (following the text:mode:id format) (ex.: Sample role:toggle:796899707045543946, Add-only role:addonly:796899707045543946)>"),
-
-	async init({ log }) {
-		try {
-			enabled = require("./config.json").allowNewPickers;
-		} catch {
-			enabled = true;
-			log("Auto roles", localizer._("Configuration file not found or invalid. The createrolepicker command is enabled by default."), "warn");
-		};
-	},
-
-	async execute(message, args, { config }) {
-		// Check if command is enabled
-		if (!enabled) return sendErrorMessage(message.channel, config, localizer._("This command is disabled."));
-
-		// Check if user has admin permissions
-		if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return sendErrorMessage(message.channel, config,
-			localizer._("You must be an administrator to use this command.")
-		);
-
-		// Check if there is an argument
-		if (!args.length) return sendErrorMessage(message.channel, config, localizer._("No arguments! Use the `help createrolepicker` command to learn more."));
-
-		const row = new MessageActionRow();
-		args = args.join(" ").split(", ");
-
-		args.slice(0, 5).forEach(arg => {
-			const settings = arg.split(":");
-
-			if (settings.length != 3 || !message.guild.roles.cache.get(settings[2]) || !allowedModes.includes(settings[1])) {
-				return sendErrorMessage(
-					message.channel,
-					config,
-					localizer.__("[[0]] is not valid.", { placeholders: [arg] })
-				);
-			};
-
-			row.addComponents(
-				new MessageButton()
-					.setLabel(settings[0])
-					.setStyle("PRIMARY")
-					.setCustomId(`autorole-${settings[1]}-${settings[2]}`)
-			);
-		});
-
-		await message.channel.send({
-			components: [row]
-		});
-
-		message.delete();
+/**
+ * Initialize createRolePicker command
+ * @param {Artibot} artibot 
+ */
+export async function createRolePickerInit({ log, config }) {
+	try {
+		enabled = config.autoroles.allowNewPickers;
+	} catch {
+		enabled = true;
+		log("Auto roles", localizer._("Configuration not found or invalid. The createrolepicker command is enabled by default."), "log");
 	}
+}
+
+channel.send({
+	embeds: [
+		createEmbed()
+			.setColor("RED")
+			.setTitle("Autorole")
+			.setDescription(reason)
+	]
+});
+
+/**
+ * Command to create a role picker
+ * @author GoudronViande24
+ * @since 2.0.0
+ * @param {Message} message
+ * @param {string[]} args
+ * @param {Artibot} artibot
+ */
+export async function createRolePicker(message, args, { config, createEmbed }) {
+	// Check if command is enabled
+	if (!enabled) return message.channel.send({
+		embeds: [
+			createEmbed()
+				.setColor("RED")
+				.setTitle("Autorole")
+				.setDescription(localizer._("This command is disabled."))
+		]
+	});
+
+	// Check if user has admin permissions
+	if (!message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return message.channel.send({
+		embeds: [
+			createEmbed()
+				.setColor("RED")
+				.setTitle("Autorole")
+				.setDescription(localizer._("You must be an administrator to use this command."))
+		]
+	});
+
+	// Check if there is an argument
+	if (!args.length) return message.channel.send({
+		embeds: [
+			createEmbed()
+				.setColor("RED")
+				.setTitle("Autorole")
+				.setDescription(localizer._("No arguments! Use the `help createrolepicker` command to learn more."))
+		]
+	});
+
+	const row = new MessageActionRow();
+	args = args.join(" ").split(", ");
+
+	args.slice(0, 5).forEach(arg => {
+		const settings = arg.split(":");
+
+		if (settings.length != 3 || !message.guild.roles.cache.get(settings[2]) || !allowedModes.includes(settings[1])) {
+			return sendErrorMessage(
+				message.channel,
+				config,
+				localizer.__("[[0]] is not valid.", { placeholders: [arg] })
+			);
+		}
+
+		row.addComponents(
+			new MessageButton()
+				.setLabel(settings[0])
+				.setStyle("PRIMARY")
+				.setCustomId(`autorole-${settings[1]}-${settings[2]}`)
+		);
+	});
+
+	await message.channel.send({
+		components: [row]
+	});
+
+	await message.delete();
 }
--- index.js
+++ index.js
@@ -1,33 +1,109 @@
+import Artibot, { Button, Command, Global, Module } from "artibot";
+import Localizer from "artibot-localizer";
+import { CommandInteraction, Intents } from "discord.js";
+import { ReactionRole } from "discordjs-reaction-role";
+import { createRolePicker, createRolePickerInit } from "./createrolepicker.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from 'module';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const require = createRequire(import.meta.url);
+const { version } = require('./package.json');
+
 /**
  * Autoroles module for Artibot
  * Partly based on Node.js module discordjs-reaction-role
  * @author GoudronViande24
+ * @param {Artibot} artibot
+ * @returns {Module}
  */
+export default ({ config: { lang } }) => {
+	localizer.setLocale(lang);
 
-module.exports = {
-	manifest: {
-		manifestVersion: 1,
-		moduleVersion: "2.0.0",
+	return new Module({
+		id: "autoroles",
 		name: "Auto roles",
-		supportedLocales: ["en", "fr"],
+		version,
+		langs: [
+			"en",
+			"fr"
+		],
+		repo: "GoudronViande24/artibot-autoroles",
+		intents: [
+			Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+		],
 		parts: [
-			{
+			new Global({
 				id: "reactionrole",
-				type: "global",
-				path: "reactionroles.js"
-			},
-
-			{
+				mainFunction: ({ client, config }) => new ReactionRole(client, config.autoroles.reactionRoles)
+			}),
+			new Button({
 				id: "autorole-*",
-				type: "button",
-				path: "rolebutton.js"
-			},
-
-			{
+				mainFunction: autoroleButton
+			}),
+			new Command({
 				id: "createrolepicker",
-				type: "command",
-				path: "createrolepicker.js"
-			}
+				name: "createrolepicker",
+				description: localizer._("Create a role picker."),
+				usage: localizer._("<list of buttons to create (following the text:mode:id format) (ex.: Sample role:toggle:796899707045543946, Add-only role:addonly:796899707045543946)>"),
+				guildOnly: true,
+				requiresArgs: true,
+				initFunction: createRolePickerInit,
+				mainFunction: createRolePicker
+			})
 		]
+	})
+}
+
+export const localizer = new Localizer({
+	filePath: path.join(__dirname, "locales.json")
+});
+
+/**
+ * Give or remove role to user when button clicked
+ * @since 2.0.0
+ * @param {CommandInteraction} interaction 
+ * @param {Artibot} artibot
+ */
+async function autoroleButton(interaction, { createEmbed }) {
+	const roleId = interaction.customId.split("-")[2],
+		mode = interaction.customId.split("-")[1];
+
+	const embed = createEmbed().setTitle("Autorole");
+
+	const role = await interaction.guild.roles.fetch(roleId);
+
+	switch (mode) {
+		case "toggle":
+			if (interaction.member.roles.cache.has(roleId)) {
+				await interaction.member.roles.remove(roleId);
+				embed.setDescription(localizer.__("You no longer have the [[0]] role.", { placeholders: [role.name] }));
+			} else {
+				await interaction.member.roles.add(roleId);
+				embed.setDescription(localizer.__("You now have the [[0]] role.", { placeholders: [role.name] }));
+			};
+			break;
+
+		case "addonly":
+			await interaction.member.roles.add(roleId);
+			embed.setDescription(localizer.__("You now have the [[0]] role.", { placeholders: [role.name] }));
+			break;
+
+		case "removeonly":
+			await interaction.member.roles.remove(roleId);
+			embed.setDescription(localizer.__("You no longer have the [[0]] role.", { placeholders: [role.name] }));
+			break;
+
+		default:
+			throw new Error(localizer._("Requested mode is not valid!"));
 	}
-};
+
+	await interaction.reply({
+		embeds: [embed],
+		ephemeral: true
+	});
+}
